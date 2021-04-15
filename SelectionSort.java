5b819f480db35d39ec25a9f38e980e3a7878a70c
Karim <aksharif@umich.edu>
2021-04-15 11:40:08 -0400

    Pos/Neg

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,4 +1,34 @@
 
-public class SelectionSort {
+public class SelectionSort
+{
+	private int temp;
 
+	/** Creates a new instance of SelectionSort */
+	public SelectionSort() {}
+
+	/* A simple SelectionSort algorithm
+	 * pre-condition:
+	 * post-condition:
+	 * inputs:
+	 * outputs:
+	 * special conditions:
+	 */
+
+	public int[] basicSelectionSort(int[] x)
+	{
+		for (int i = 0; i < x.length-1; ++i)
+		{
+			for (int j= i+1; j < x.length; ++j)
+			{
+				if (x[j] < x[i])
+				{
+					temp = x[j];
+					x[j] = x[i];
+					x[i] = temp;
+				}
+			} // end of inner for loop
+		} // end of outer for loop
+
+	return x;
+	} // end of basicSelectionSort method}
 }
--- testSelectionSort.java
+++ testSelectionSort.java
@@ -1,12 +1,75 @@
-import static org.junit.jupiter.api.Assertions.*;
+/*
+ * testSelectionSort.java
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-class testSelectionSort {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+public class testSelectionSort
+{
+	@Test public void test()
+	{
+		testPositive();
+		testNegative();
 	}
 
+	public testSelectionSort() {}
+
+	public void testPositive()
+	{
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+
+		int[] Sortedarr = new int[5];
+
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
+
+		SelectionSort sort = new SelectionSort();
+
+		arr = sort.basicSelectionSort(arr);
+
+		for (int i = 0; i < 5; i++)
+		{
+        	assertTrue("Element sorted",arr[i] == Sortedarr[i]);
+		}
+	}
+
+	public void testNegative()
+	{
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = -7;
+		arr[3] = -10;
+		arr[4] = -2;
+
+		int[] Sortedarr = new int[5];
+
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -9;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = -7;
+		Sortedarr[4] = -2;
+
+		SelectionSort sort = new SelectionSort();
+
+		arr = sort.basicSelectionSort(arr);
+
+		for (int i = 0; i < 5; i++)
+		{
+					assertTrue("Element sorted",arr[i] == Sortedarr[i]);
+		}
+	}
 }
