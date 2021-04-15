ce7a8f6e698e1f37e7c70af15c3a9f12736a86fd
Karim <aksharif@umich.edu>
2021-04-15 11:48:23 -0400

    commiting

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-public class SelectionSort 
-{
-	private int temp;
-	
-	/** Creates a new instance of SelectionSort */
-	public SelectionSort() {}
-	
-	/* A simple SelectionSort algorithm
-	 * pre-condition:
-	 * post-condition:     
-	 * inputs:     
-	 * outputs:     
-	 * special conditions:     
-	 */    
-	
-	public int[] basicSelectionSort(int[] x) 
-	{
-		for (int i = 0; i < x.length; ++i) 
-		{
-			for (int j= i+1; j < x.length; ++j) 
-			{
-				if (x[i] > x[j]) 
-				{
-					temp = x[i];
-					x[i] = x[j];
-					temp = x[j];
-				}
-			} // end of inner for loop
-		} // end of outer for loop
-		
-		return x;    
-	} // end of basicSelectionSort method
-=======
 
 public class SelectionSort
 {
@@ -58,20 +24,11 @@ public class SelectionSort
 				{
 					temp = x[j];
 					x[j] = x[i];
-<<<<<<< HEAD
-					x[i] = temp;  
-=======
 					x[i] = temp;
->>>>>>> Pos/Neg
 				}
 			} // end of inner for loop
 		} // end of outer for loop
 
 	return x;
 	} // end of basicSelectionSort method}
-<<<<<<< HEAD
->>>>>>> Mix/Dup
-=======
->>>>>>> Pos/Neg
 }
-	
--- testSelectionSort.java
+++ testSelectionSort.java
@@ -13,112 +13,12 @@ public class testSelectionSort
 {
 	@Test public void test()
 	{
-<<<<<<< HEAD
-<<<<<<< HEAD
-		testPositive();
-		testNegative();
 		testMixed();
 		testDuplicates();
-	}
-
-	private void testDuplicates() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void testMixed() {
-		// TODO Auto-generated method stub
-		
-=======
-		testMixed();
-		testDuplicates();
->>>>>>> Mix/Dup
-=======
-		testPositive();
-		testNegative();
->>>>>>> Pos/Neg
 	}
 
 	public testSelectionSort() {}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Pos/Neg
-	public void testPositive()
-	{
-		int[] arr = new int[5];
-		arr[0] = 8;
-		arr[1] = 9;
-		arr[2] = 7;
-<<<<<<< HEAD
-		arr[3] = -10;
-		arr[4] = -2;
-
-		int[] Sortedarr = new int[5];
-
-		Sortedarr[0] = -10;
-		Sortedarr[1] = -2;
-		Sortedarr[2] = 7;
-		Sortedarr[3] = 8;
-		Sortedarr[4] = 9;
-=======
-		arr[3] = 10;
-		arr[4] = 2;
-
-		int[] Sortedarr = new int[5];
-
-		Sortedarr[0] = 2;
-		Sortedarr[1] = 7;
-		Sortedarr[2] = 8;
-		Sortedarr[3] = 9;
-		Sortedarr[4] = 10;
->>>>>>> Pos/Neg
-
-		SelectionSort sort = new SelectionSort();
-
-		arr = sort.basicSelectionSort(arr);
-<<<<<<< HEAD
-
-		for (int i = 0; i < 5; i++)
-		{
-        	assertTrue("Element sorted",arr[i] == Sortedarr[i]);
-		}
-	}
-
-	public void testNegative()
-	{
-		int[] arr = new int[5];
-		arr[0] = 8;
-		arr[1] = 8;
-		arr[2] = 7;
-		arr[3] = 10;
-		arr[4] = 2;
-
-		int[] Sortedarr = new int[5];
-
-		Sortedarr[0] = 2;
-		Sortedarr[1] = 7;
-		Sortedarr[2] = 8;
-		Sortedarr[3] = 8;
-		Sortedarr[4] = 10;
-
-		SelectionSort sort = new SelectionSort();
-
-		arr = sort.basicSelectionSort(arr);
-
-		for (int i = 0; i < 5; i++)
-		{
-					assertTrue("Element sorted",arr[i] == Sortedarr[i]);
-=======
-
-		for (int i = 0; i < 5; i++)
-		{
-        	assertTrue("Element sorted",arr[i] == Sortedarr[i]);
->>>>>>> Pos/Neg
-		}
-	}
-=======
 		public void testDuplicates() {
 			int[] arr = new int[5];
 			arr[0] = 8;
@@ -166,39 +66,9 @@ public class testSelectionSort
 
 			arr = sort.basicSelectionSort(arr);
 
-<<<<<<< HEAD
 			for (int i = 0; i < 5; i++)
 			{
 				assertTrue("Element sorted",arr[i] == Sortedarr[i]);
 			}
 		}
-=======
-	public void testNegative()
-	{
-		int[] arr = new int[5];
-		arr[0] = -8;
-		arr[1] = -9;
-		arr[2] = -7;
-		arr[3] = -10;
-		arr[4] = -2;
-
-		int[] Sortedarr = new int[5];
-
-		Sortedarr[0] = -10;
-		Sortedarr[1] = -9;
-		Sortedarr[2] = -8;
-		Sortedarr[3] = -7;
-		Sortedarr[4] = -2;
-
-		SelectionSort sort = new SelectionSort();
-
-		arr = sort.basicSelectionSort(arr);
-
-		for (int i = 0; i < 5; i++)
-		{
-					assertTrue("Element sorted",arr[i] == Sortedarr[i]);
-		}
-	}
->>>>>>> Pos/Neg
 }
->>>>>>> Mix/Dup
