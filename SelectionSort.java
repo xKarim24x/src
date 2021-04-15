cf96aa92275e17b4bac72613432882722cb4e0d8
Karim <aksharif@umich.edu>
2021-04-15 11:34:40 -0400

    Sort fix

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,35 +1,34 @@
 
-public class SelectionSort 
+public class SelectionSort
 {
 	private int temp;
-	
+
 	/** Creates a new instance of SelectionSort */
-	public SelectionSort() {} 
-	
+	public SelectionSort() {}
+
 	/* A simple SelectionSort algorithm
-	 * pre-condition:     
-	 * post-condition:     
-	 * inputs:     
-	 * outputs:     
-	 * special conditions:     
+	 * pre-condition:
+	 * post-condition:
+	 * inputs:
+	 * outputs:
+	 * special conditions:
 	 */
-	
-	public int[] basicSelectionSort(int[] x) 
+
+	public int[] basicSelectionSort(int[] x)
 	{
-		for (int i = 0; i < x.length; ++i) 
+		for (int i = 0; i < x.length-1; ++i)
 		{
-			for (int j= i+1; j < x.length; ++j) 
+			for (int j= i+1; j < x.length; ++j)
 			{
-				if (x[i] > x[j]) 
-				{           
-					temp = x[i];
-					x[i] = x[j];
-					temp = x[j];  
+				if (x[j] < x[i])
+				{
+					temp = x[j];
+					x[j] = x[i];
+					x[i] = temp;  
 				}
-			} // end of inner for loop 
+			} // end of inner for loop
 		} // end of outer for loop
-		
+
 	return x;
 	} // end of basicSelectionSort method}
 }
-
