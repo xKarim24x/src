14a90255a20d30a6d5427a6b1fb39d1a0e04e996
Karim <aksharif@umich.edu>
2021-04-15 11:53:15 -0400

    final commit

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -13,6 +13,8 @@ public class testSelectionSort
 {
 	@Test public void test()
 	{
+		testPositive();
+		testNegative();
 		testMixed();
 		testDuplicates();
 	}
@@ -71,4 +73,58 @@ public class testSelectionSort
 				assertTrue("Element sorted",arr[i] == Sortedarr[i]);
 			}
 		}
+}
+
+public void testPositive()
+{
+	int[] arr = new int[5];
+	arr[0] = 8;
+	arr[1] = 9;
+	arr[2] = 7;
+	arr[3] = 10;
+	arr[4] = 2;
+
+	int[] Sortedarr = new int[5];
+
+	Sortedarr[0] = 2;
+	Sortedarr[1] = 7;
+	Sortedarr[2] = 8;
+	Sortedarr[3] = 9;
+	Sortedarr[4] = 10;
+
+	SelectionSort sort = new SelectionSort();
+
+	arr = sort.basicSelectionSort(arr);
+
+	for (int i = 0; i < 5; i++)
+	{
+				assertTrue("Element sorted",arr[i] == Sortedarr[i]);
+	}
+}
+
+public void testNegative()
+{
+	int[] arr = new int[5];
+	arr[0] = -8;
+	arr[1] = -9;
+	arr[2] = -7;
+	arr[3] = -10;
+	arr[4] = -2;
+
+	int[] Sortedarr = new int[5];
+
+	Sortedarr[0] = -10;
+	Sortedarr[1] = -9;
+	Sortedarr[2] = -8;
+	Sortedarr[3] = -7;
+	Sortedarr[4] = -2;
+
+	SelectionSort sort = new SelectionSort();
+
+	arr = sort.basicSelectionSort(arr);
+
+	for (int i = 0; i < 5; i++)
+	{
+		assertTrue("Element sorted",arr[i] == Sortedarr[i]);
+	}
 }
