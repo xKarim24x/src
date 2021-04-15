b2bfed511b7c6bf6dbe366fe761f825f7fe3a913
Karim <aksharif@umich.edu>
2021-04-15 11:16:33 -0400

    commit 3

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -1,12 +1,86 @@
-import static org.junit.jupiter.api.Assertions.*;
+/*
+ * testSelectionSort.java
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-class testSelectionSort {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+public class testSelectionSort
+{
+	@Test public void test()
+	{
+		testPositive();
+		testNegative();
+		testMixed();
+		testDuplicates();
 	}
 
-}
+	private void testDuplicates() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void testMixed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public testSelectionSort() {}
+
+	public void testPositive()
+	{
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = -10;
+		arr[4] = -2;
+
+		int[] Sortedarr = new int[5];
+
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -2;
+		Sortedarr[2] = 7;
+		Sortedarr[3] = 8;
+		Sortedarr[4] = 9;
+
+		SelectionSort sort = new SelectionSort();
+
+		arr = sort.basicSelectionSort(arr);
+
+		for (int i = 0; i < 5; i++)
+		{
+        	assertTrue("Element sorted",arr[i] == Sortedarr[i]);
+		}
+	}
+
+	public void testNegative()
+	{
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 8;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+
+		int[] Sortedarr = new int[5];
+
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 8;
+		Sortedarr[4] = 10;
+
+		SelectionSort sort = new SelectionSort();
+
+		arr = sort.basicSelectionSort(arr);
+
+		for (int i = 0; i < 5; i++)
+		{
+					assertTrue("Element sorted",arr[i] == Sortedarr[i]);
+		}
+	}
